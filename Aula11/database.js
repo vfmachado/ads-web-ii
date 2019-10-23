d486450b9dd142017ae74f63cbbdf1aeeafb3ab5
vinicius <fritzenvinicius@gmail.com>
2019-10-23 14:41:18 -0300

    Setup Database

--- Aula11/database.js
+++ Aula11/database.js
@@ -2,9 +2,9 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createPool({
-    host: '200.132.13.90',  //onde está o meu banco
-    user: 'roger',       //usuario
-    password: 'roger',   //senha
+    host: 'localhost',  //onde está o meu banco
+    user: 'admin',       //usuario
+    password: 'admin',   //senha
     database: 'ads-progweb-ii'    //qual o esquema
 }).promise();
 
