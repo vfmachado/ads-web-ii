36832a0e32981e1ba5e833bd75be9409118df396
vinicius <fritzenvinicius@gmail.com>
2019-10-28 22:22:52 -0300

    Mongoose e Upload de Arquivos

---
+++ Aula13/controllers/userController.js
@@ -0,0 +1,35 @@
+
+const Product = require('../models/Product');
+
+
+exports.MostraPaginaInicial = (req, res) => {
+     
+    Product.find()
+    .then(resultado => {
+
+        res.render('todos', {
+            pageTitle: "IFRS Eletrônicos",
+            prods: resultado,
+            logado: req.session.loggedIn,
+            user : req.session.user})
+
+    })
+    
+
+}
+
+
+exports.Detalhar = (req, res) => {
+    console.log(req.session.user);
+   Product.findById(req.params.id).then(prod => {
+    res.render('detalhar', 
+    {
+        pageTitle: "IFRS Eletrônicos - Detalhe",
+        product: prod,
+        logado: req.session.loggedIn,
+        user : req.session.user
+    });
+   })
+    
+
+}
