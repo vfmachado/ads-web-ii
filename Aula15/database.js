e752b28865b2717218af9ecace7eb0ece427241c
Vinicius Machado <fritzenvinicius@gmail.com>
2019-11-16 00:12:57 -0300

    Melhorias no CSS

--- Aula15/database.js
+++ Aula15/database.js
@@ -2,9 +2,9 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createPool({
-    host: '',  //onde está o meu banco
-    user: 'roger',       //usuario
-    password: 'roger',   //senha
+    host: 'localhost',  //onde está o meu banco
+    user: 'admin',       //usuario
+    password: 'admin',   //senha
     database: 'ads-progweb-ii'    //qual o esquema
 }).promise();
 
